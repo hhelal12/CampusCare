75f7d8a6c7a4af0a146f4738363ea3aa85e0798b
hhelal12 <hlhols313@gmail.com>
2025-11-25 15:02:27 +0300

    create firebase connection

--- CampusCare/Firebase/FirestoreManager.swift
+++ CampusCare/Firebase/FirestoreManager.swift
@@ -5,11 +5,10 @@ final class FirestoreManager {
     // Shared instance for easy access across the app
     static let shared = FirestoreManager()
     
-    // The private, immutable reference to the database instance
     let db: Firestore
     
     private init() {
-        // Initializes the reference when the shared instance is first accessed
+   
         self.db = Firestore.firestore()
     }
 }
